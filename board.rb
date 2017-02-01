9122339f1de7b38a730a2a44b56356ba0b145cec
jennalarcon <alarcon.11@osu.edu>
2017-02-01 16:06:26 -0500

    Added proper documentation layout

--- board.rb
+++ board.rb
@@ -2,15 +2,19 @@
 #Creation Date: 1/30/17
 
 
-require_relative './card'
-require_relative './deck'
+require_relative 'card'
+require_relative 'deck'
 STARTINGCARDS = 12
+
 class Board < Deck
 	#deck and card can only be read
 	attr_reader :card, :deck
 
 	# Method Author: Kenton Steiner - 1/30/17
-	# Initialize the board array, deck, and create the starting board
+	# Description: Initialize the board array, deck, and create the starting board
+	# Team Member           Date           Changes
+	#
+
 	def initialize(cards = [])
 		@board = cards
 		@deck = :deck
@@ -27,14 +31,19 @@ class Board < Deck
 	end
 
 	#Author: Kenton Steiner - 2/1/17
-	# Returns an array of the indices of the cards to check for a set
+	#Description: Returns an array of the indices of the cards to check for a set
+	# Team Member           Date           Changes
+	#
 	def indicies_of(set)
 		indices = []
 		set.each {|card| indices << set.index(card) }
 	end
 
 	# Method Author: Kenton Steiner - 2/1/17
-	def is_set at?(indices)
+	# Description:
+	# Team Member           Date           Changes
+	#
+	def is_set(indices)
 		potential_set = self.cards_at(indices)
 		# actual_set = Card::is_this_a_set?(potential_set) # Need to create this function
 		puts "This is not a set! Try again!" unless actual_set
@@ -42,20 +51,27 @@ class Board < Deck
 
 
 	# Method Author: Kenton Steiner - 2/1/17
-	#Takes an array of indices (a set), and returns the cards at those indices
+	#Description: Takes an array of indices (a set), and returns the cards at those indices
+	# Team Member           Date           Changes
+	#
 	def cards_at(indices)
 		cards = []
 		indices.each {|x| cards << @board[x] }
 		cards
 	end
+
 	# Method Author: Kenton Steiner - 2/1/17
-	#Takes an array of indices, the set found, and deletes the cards from the board
+	# Decription: Takes an array of indices, the set found, and deletes the cards from the board
+	# Team Member           Date           Changes
+	#
 	def remove_cards_at(indices)
 		indices.each { |x| @board.delete_at(x) }
 	end
 
 	# Method Author: Kenton Steiner - 2/1/17
-	# Adds 3 new cards to the board
+	# Description: Adds 3 new cards to the board
+	# Team Member           Date           Changes
+	# 
 	def add_cards
 		3.times { @board.push(@deck.draw) }
 	end
